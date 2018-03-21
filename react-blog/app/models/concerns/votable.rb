736a15524b961b7cee9b6e575f238bd8267bcbb5
“shilberry” <moreta11706@live.com>
2018-03-21 17:38:43 -0400

    syncing directory

--- react-blog/app/models/concerns/votable.rb
+++
@@ -1,9 +0,0 @@
-module Votable
-
-  extend ActiveSupport::Concern
-
-  def voted? user_id
-    voter_ids.include? user_id.to_s
-  end
-
-end
--- react-blog/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root to: 'pages#home'
-  devise_for :users, skip: [:registrations],  controllers: {registrations: 'users/registrations'}
-  as :user do
-    get   '/signup' => 'users/registrations#new',    as: 'new_user_registration'
-    post  '/signup' => 'users/registrations#create', as: 'user_registration'
-  end
-  resources :posts
-end

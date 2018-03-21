159a886f2bca1dece01f3c210ebc47df79901f87
“shilberry” <moreta11706@live.com>
2018-03-21 17:47:36 -0400

    merging

---
+++ react-blog/app/assets/javascripts/application.js
@@ -0,0 +1,11 @@
+//= require_self
+//= require app
+//= require js-routes
+//= require local_time
+//= require environment
+//= require current_user
+//= require components
+
+// Setup React in global scope
+window.$ = window.jQuery = require('jquery')
+require('jquery-ujs')
